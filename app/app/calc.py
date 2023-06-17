7d4a1b5105a7e8d118f1ff19ba4ba608dd0648ea
SyedSalman007 <97502374+SyedSalman007@users.noreply.github.com>
2023-06-18 02:01:08 +0500

    second_commit

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculate Functions
+"""
+
+def add(x, y):
+    """ Add two number and then return the result """
+    return x+y
+
+def subtract(x, y):
+    """ Subtract two numbers and then return the result"""
+    return x-y 
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample Test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test case of calc module"""
+
+    def test_add_number(self):
+       """Test adding number together"""
+       res = calc.add(5,6)
+
+       self.assertEqual(res, 11)
+
+    
+    def test_sub_number (self):
+        """Test subtracting numbers together"""
+        res = calc.subtract(10,5)
+
+        self.assertEqual(res, 5)
